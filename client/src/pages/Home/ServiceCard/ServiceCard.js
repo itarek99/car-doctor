e3b0d657553ad01645e1ef5b0b158809a1c576a6
itarek99 <tarekulislam7060@gmail.com>
2022-11-02 19:25:25 +0600

    services section complete

---
+++ client/src/pages/Home/ServiceCard/ServiceCard.js
@@ -0,0 +1,21 @@
+const ServiceCard = ({ service }) => {
+  const { img, title, price } = service;
+
+  console.log(service);
+
+  return (
+    <div className='card bg-base-100 shadow-xl rounded-none border'>
+      <figure>
+        <img className='h-60 object-cover w-full' src={img} alt={title} />
+      </figure>
+      <div className='card-body'>
+        <h2 className='card-title text-2xl font-bold'>{title}</h2>
+        <button className='flex justify-between items-center'>
+          <p className='text-xl font-semibold text-primary text-start'>Price: {price}</p>
+          <span className=' btn-link no-underline text-xl'>❯</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+export default ServiceCard;
--- client/src/pages/Home/Services/Services.js
+++ client/src/pages/Home/Services/Services.js
@@ -1,4 +1,16 @@
+import { useEffect, useState } from 'react';
+import ServiceCard from '../ServiceCard/ServiceCard';
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch('services.json')
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  console.log(services);
+
   return (
     <div className='container mx-auto mb-20 px-2'>
       <div className='text-center'>
@@ -6,8 +18,14 @@ const Services = () => {
         <h2 className='font-bold text-5xl mt-3'>Our Services Area</h2>
         <p className='w-full md:w-3/4 xl:w-1/2 mx-auto mt-5'>
           the majority have suffered alteration in some form, by injected humour, or randomised words which don't look
-          even slightly believable.{' '}
+          even slightly believable.
         </p>
+      </div>
+
+      <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 mt-12'>
+        {services.map((item) => (
+          <ServiceCard service={item} />
+        ))}
       </div>
     </div>
   );
